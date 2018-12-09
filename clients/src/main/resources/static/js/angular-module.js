13a6f3b56ea1f1450caea39e2443d738168881e0
AK <akihidis@gmail.com>
2018-12-09 09:15:25 +0000

    Added alert error support on the create game popup

--- clients/src/main/resources/static/js/angular-module.js
+++ clients/src/main/resources/static/js/angular-module.js
@@ -1,10 +1,15 @@
 "use strict";
 
 $(document).ready(function() {
+    var signaturesConsole = $("#signaturesPanel");
+    var messagePopup = $('#messagePopup');
+    var createGamePopupErrorAlert = $("#createGameErrorAlert");
+
     var apiBaseURL = "http://localhost:10015/api/";
     var peers = new Array();
 
     $('#createGameModal').modal({show: false});
+    $('#messagePopup').modal({show: false});
 
     $("#newGameBtn").click(function() {
         //render opponents options
@@ -25,6 +30,8 @@ $(document).ready(function() {
 
 
     $("#modalCreateGameBtn").click(function() {
+        $("#createGameErrorAlert .alertText").html("");
+
         var opponentX500Name = $("#opponents").val();
         var nickname = $("#nickname").val();
 
@@ -40,9 +47,29 @@ $(document).ready(function() {
             data: JSON.stringify(postData),
             success: function(data) {
                 console.log(data);
+
+                $("#createGameModal").modal('hide');
+
+                messagePopup.find(".text").html(JSON.stringify(data));
+                messagePopup.modal('show');
+
+                signaturesConsole.find("option").remove();
+            },
+            error: function(data) {
+                createGamePopupErrorAlert.find(".alertText").html(JSON.stringify(data));
+
+                createGamePopupErrorAlert.alert('show');
             }
         });
     });
+
+
+    var toggleCreateGameErrorAlert = function(){
+        createGamePopupErrorAlert.toggleClass('hide');
+        return false; // Keep close.bs.alert event from removing from DOM
+    }
+
+    createGamePopupErrorAlert.on('close.bs.alert', toggleCreateGameErrorAlert);
 
     var retrieveNickname = function() {
         $.get(apiBaseURL + "me", function(data) {
@@ -57,6 +84,13 @@ $(document).ready(function() {
             peers = data.peers;
             console.log(peers);
         });
+    }
+
+    var appendToSignaturesConsole = function(signature) {
+        signaturesConsole.append($('<option>', {
+                        value: signature,
+                        text: signature
+                    }));
     }
 
     retrieveNickname();
